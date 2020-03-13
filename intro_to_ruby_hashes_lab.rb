7d12fe2b75eab73d4f20b643b965eb97c05e7641
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-13 03:43:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ end
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-end
+
 
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
